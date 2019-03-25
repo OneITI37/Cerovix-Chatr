4f96b70a3573ad2be26aae44285f7a8ae117bae1
OneITI37 <kjhstudio65@gmail.com>
2019-03-25 16:04:29 +0900

    Update signin-helper.js
    Added color transition features

--- signin-helper.js
+++ signin-helper.js
@@ -11,6 +11,18 @@ function initialSequences() {
     fadeAnimation(document.getElementById("identifier-section"), 0.00, 1, 1);
     document.getElementById("identifier").readOnly = false;
     isIdentifierActive = true;
+    document.getElementById("identifier").addEventListener("focus", function(){
+        backgroundColorTransitionAnimation(document.getElementById("identifier"), 255, 255, 255, 0, 120, 215, 0.3);
+    });
+    document.getElementById("identifier").addEventListener("blur", function(){
+        backgroundColorTransitionAnimation(document.getElementById("identifier"), 0, 120, 215, 255, 255, 255, 0.3);
+    });
+    document.getElementById("password").addEventListener("focus", function(){
+        backgroundColorTransitionAnimation(document.getElementById("password"), 255, 255, 255, 0, 120, 215, 0.3);
+    });
+    document.getElementById("password").addEventListener("blur", function(){
+        backgroundColorTransitionAnimation(document.getElementById("password"), 0, 120, 215, 255, 255, 255, 0.3);
+    });
     fadeAnimation(document.getElementById("password-section"), 0.00, 0.10, 1);
     fadeAnimation(document.getElementById("login-button"), 0.00, 0.10, 1);
     document.getElementById("identifier").addEventListener("keyup", function(){
@@ -78,6 +90,34 @@ function fadeAnimation(target_element, start_opacity, end_opacity, animation_dur
     var interval_sequences = 0;
     var animation_interval = setInterval(function(){
         target_element.style.opacity = (Number(target_element.style.opacity) + (end_opacity-start_opacity)/(animation_duration*100)).toString(10);
+        if (++interval_sequences >= animation_duration * 100)
+            clearInterval(animation_interval);
+    }, 10);
+    return;
+}
+function backgroundColorTransitionAnimation(target_element, start_color_red, start_color_green, start_color_blue, end_color_red, end_color_green, end_color_blue, animation_duration) {
+    target_element.style.backgroundColor = "rgb("+start_color_red+", "+start_color_green+", "+start_color_blue+")";
+    var interval_sequences = 0;
+    var current_red = start_color_red, current_green = start_color_green, current_blue = start_color_blue;
+    var animation_interval = setInterval(function(){
+        current_red += (end_color_red-start_color_red)/(animation_duration*100);
+        current_green += (end_color_green-start_color_green)/(animation_duration*100);
+        current_blue += (end_color_blue-start_color_blue)/(animation_duration*100);
+        target_element.style.backgroundColor = "rgb("+current_red+", "+current_green+", "+current_blue+")";
+        if (++interval_sequences >= animation_duration * 100)
+            clearInterval(animation_interval);
+    }, 10);
+    return;
+}
+function textColorTransitionAnimation(target_element, start_color_red, start_color_green, start_color_blue, end_color_red, end_color_green, end_color_blue, animation_duration) {
+    target_element.style.color = "rgb("+start_color_red+", "+start_color_green+", "+start_color_blue+")";
+    var interval_sequences = 0;
+    var current_red = start_color_red, current_green = start_color_green, current_blue = start_color_blue;
+    var animation_interval = setInterval(function(){
+        current_red += (end_color_red-start_color_red)/(animation_duration*100);
+        current_green += (end_color_green-start_color_green)/(animation_duration*100);
+        current_blue += (end_color_blue-start_color_blue)/(animation_duration*100);
+        target_element.style.color = "rgb("+current_red+", "+current_green+", "+current_blue+")";
         if (++interval_sequences >= animation_duration * 100)
             clearInterval(animation_interval);
     }, 10);
