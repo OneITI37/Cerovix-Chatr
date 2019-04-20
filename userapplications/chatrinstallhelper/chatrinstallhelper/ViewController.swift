ef9cf93166fc5811307dd14d05cbb438a6aa53c9
Hoyoun Song <hysong@zeone.kro.kr>
2019-04-20 16:06:32 +0900

    Update Helper added

---
+++ userapplications/chatrinstallhelper/chatrinstallhelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  chatrinstallhelper
+//
+//  Created by Hoyoun Song on 20/04/2019.
+//  Copyright © 2019 Zeone. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
